7c666ff7f635b4f557036e5de26a68bc8de4c82e
KarasDominik <dominik.karas99@gmail.com>
2023-07-25 16:51:53 +0200

    Added register and log in buttons

--- ProgressTracker/frontend/src/App.js
+++ ProgressTracker/frontend/src/App.js
@@ -1,13 +1,34 @@
-import React, {Component} from 'react';
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Registration from './Registration.js';
+import Logging from './Logging.js';
+import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 
-import Registration from './Registration.js'
-import Logging from './Logging.js'
-
-class App extends Component{
+class App extends Component {
   render() {
-    return <Registration />
+    return (
+      <Router>
+        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/register">Register</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+        <Routes>
+          <Route path="/login" element={<Logging />} />
+          <Route path="/register" element={<Registration />} />
+        </Routes>
+      </Router>
+    );
   }
 }
 
